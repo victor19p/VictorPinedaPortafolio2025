39ba212a90a41f2fbedbeffc8a18cc9d06e3610f
Victor Pineda <victor19cod@gmail.com>
2025-05-10 18:01:37 -0600

    Portafolio init
    toggle darkmode
    star background

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import { ThemeToggle } from "../components/ThemeToggle";
+import { StarBackground } from "./StarBackground";
+export const Home = () => {
+    return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+    {/*Theme Toggle*/  }
+    <ThemeToggle />
+    {/*Background Effect*/  }
+    <StarBackground />
+
+    {/*Navbar*/  }
+
+
+    {/*Main Content*/  }
+
+
+    {/*Footer*/  }
+
+    </div>;
+};
