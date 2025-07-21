270f105981b2a1c47178cc9fb1154ba304471d58
Victor Pineda <victor19cod@gmail.com>
2025-07-20 22:33:08 -0600

    Update v3

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,7 +4,7 @@ export const HeroSection = () => {
     return (
         <section
             id="hero"
-            className="relative min-h-screen flex flex-col items-center justify-center px-2"
+            className="relative min-h-screen flex flex-col items-center justify-center px-2 pt-16 md:pt-0"
         >
             <div className="container max-w-4xl mx-auto text-center z-10">
                 <div className="space-y-6">
